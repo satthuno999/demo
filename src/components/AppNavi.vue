8e595b3370ce4941d77504e4f1a5c2eea851469f
satthuno999 <binh9aqktk@gmail.com>
2023-04-09 08:11:24 +0700

    edit Navi2

--- src/components/AppNavi.vue
+++ src/components/AppNavi.vue
@@ -1,5 +1,69 @@
 <template>
     <!-- This component should ideally not have a conflicting name with AppNavigation from the nextcloud/vue package -->
+    <div class="left-nav-container rounded-l-3xl dark:bg-$dark_strong text-gray-500  dark:text-gray-400">
+        <div class="p-6">
+          <div class="flex justify-between items-center">
+            <CDarkMode />
+            <i class="fas fa-stream js-hide-navbar cursor-pointer"></i>
+          </div>
+          <div class="logo py-7">
+            <i class="navbar__logo fab fa-forumbee text-orange-300 text-3xl"></i>
+            <span class="navbar__primary-text text-orange-300 text-xl">SPARK</span><span class="text-xl">music</span>
+          </div>
+          <nav class="relative">
+            <li>
+              <a href="#"><i class="fas fa-home"></i>Home</a>
+            </li>
+            <li>
+              <a href="#"><i class="fas fa-chart-line"></i>Trend</a>
+            </li>
+            <li>
+              <a href="#"><i class="far fa-compass"></i>Feed</a>
+            </li>
+          </nav>
+
+          <nav class="relative">
+            <p class="text-[#9ca3af] dark:text-[#9ca3af99] text-xs">Discover</p>
+            <ul>
+              <li>
+                <a href="#"><i class="far fa-newspaper"></i>New and Notable</a>
+              </li>
+              <li>
+                <a href="#"><i class="far fa-calendar-alt"></i>Realese Calendar</a>
+              </li>
+              <li>
+                <a href="#"><i class="fas fa-ticket-alt"></i>Events</a>
+              </li>
+            </ul>
+          </nav>
+
+          <nav class="relative">
+            <p class="text-[#9ca3af] dark:text-[#9ca3af99] text-xs">Your Collection</p>
+            <ul>
+              <li>
+                <a href="#"><i class="far fa-heart"></i>Favorite Songs</a>
+              </li>
+              <li>
+                <a href="#"><i class="far fa-user"></i>Artist</a>
+              </li>
+              <li>
+                <a href="#"><i class="far fa-star"></i>Albums</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div
+          class="navbar-user flex justify-between items-center p-5 border-1 border-solid border-t-[#e9e9e9] dark:border-t-gray-600 border-b-transparent border-l-transparent border-r-transparent rounded-bl-3xl dark:bg-[#1E293B]">
+          <a href="">
+            <div class="user-avt flex items-center">
+              <img src="https://avatars.githubusercontent.com/satthuno99?v=2&s=37" alt="avatar"
+                class="rounded-full mr-2" />
+              <p class="text-sm cursor-pointer">satthuno99</p>
+            </div>
+          </a>
+          <RouterLink to="/about"><i class="fas fa-chevron-right text-gray-400"></i></RouterLink>
+        </div>
+      </div>
     <NcAppNavigation>
         <router-link :to="'/recipe/create'">
             <NcAppNavigationNew
@@ -9,7 +73,6 @@
                 <template #icon><plus-icon :size="20" /> </template>
             </NcAppNavigationNew>
         </router-link>
-
         <template slot="list">
             <NcActionInput
                 class="download"
@@ -81,70 +144,7 @@
             />
         </template>
     </NcAppNavigation>
-    <div class="left-nav-container rounded-l-3xl dark:bg-$dark_strong text-gray-500  dark:text-gray-400">
-        <div class="p-6">
-          <div class="flex justify-between items-center">
-            <!-- <CDarkMode /> -->
-            <i class="fas fa-stream js-hide-navbar cursor-pointer"></i>
-          </div>
-          <div class="logo py-7">
-            <i class="navbar__logo fab fa-forumbee text-orange-300 text-3xl"></i>
-            <span class="navbar__primary-text text-orange-300 text-xl">SPARK</span><span class="text-xl">music</span>
-          </div>
-          <nav class="relative">
-            <li>
-              <a href="#"><i class="fas fa-home"></i>Albums</a>
-            </li>
-            <li>
-              <a href="#"><i class="fas fa-chart-line"></i>Folders</a>
-            </li>
-            <li>
-              <a href="#"><i class="far fa-compass"></i>Geners</a>
-            </li>
-            <li>
-                  <a href="#"><i class="far fa-compass"></i>All tracks</a>
-            </li>
-          </nav>
-
-          <nav class="relative">
-            <p class="text-[#9ca3af] dark:text-[#9ca3af99] text-xs">Network</p>
-            <ul>
-              <li>
-                <a href="#"><i class="far fa-newspaper"></i>Internet Radio</a>
-              </li>
-              <li>
-                <a href="#"><i class="far fa-calendar-alt"></i>Podcasts</a>
-              </li>
-            </ul>
-          </nav>
-
-          <nav class="relative">
-            <p class="text-[#9ca3af] dark:text-[#9ca3af99] text-xs">Your Collection</p>
-            <ul>
-              <li>
-                <a href="#"><i class="far fa-heart"></i>Favorite Songs</a>
-              </li>
-              <li>
-                <a href="#"><i class="far fa-user"></i>Artist</a>
-              </li>
-              <li>
-                <a href="#"><i class="far fa-star"></i>Albums</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div
-          class="navbar-user flex justify-between items-center p-5 border-1 border-solid border-t-[#e9e9e9] dark:border-t-gray-600 border-b-transparent border-l-transparent border-r-transparent rounded-bl-3xl dark:bg-[#1E293B]">
-          <a href="">
-            <div class="user-avt flex items-center">
-              <img src="https://avatars.githubusercontent.com/satthuno99?v=2&s=37" alt="avatar"
-                class="rounded-full mr-2" />
-              <p class="text-sm cursor-pointer">satthuno99</p>
-            </div>
-          </a>
-          <RouterLink to="/about"><i class="fas fa-chevron-right text-gray-400"></i></RouterLink>
-        </div>
-      </div>
+    
 </template>
 
 <script>
